ef8e1b64c06010655055872d22cf85dca4fbe7f7
ajcazorla <ajcazorla@hotmail.com>
2026-02-07 12:20:41 +0100

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,39 @@
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open("control-horas-cache").then((cache) => {
-            return cache.addAll([
+const CACHE_NAME = "control-horas-v1";
+
+const URLS_TO_CACHE = [
+    "/control-horas/",
     "/control-horas/index.html",
     "/control-horas/style.css",
     "/control-horas/app.js",
     "/control-horas/manifest.json",
     "/control-horas/icon-192.png",
     "/control-horas/icon-512.png"
-])
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(URLS_TO_CACHE);
         })
     );
 });
 
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(k => k !== CACHE_NAME)
+                    .map(k => caches.delete(k))
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
